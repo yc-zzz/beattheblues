34fa4607d6eb377aa6172b7539e92e0ec4602139
yc-zzz <e1406485@u.nus.edu>
2025-06-02 12:06:29 +0800

    Update db.js

--- my-backend/db.js
+++ my-backend/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const db = mysql.createPool({
-    host: 'process.env.DB_HOST',
-    user: 'process.env.DB_USER',
-    password: 'process.env.DB_PASS',
-    database: 'process.env.DB_NAME',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 });
 
 module.exports = db;
