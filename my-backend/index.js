c047671634e06ad5cf7d0075289e4592e914cd47
yc-zzz <e1406485@u.nus.edu>
2025-06-02 12:20:52 +0800

    Update index.js

--- my-backend/index.js
+++ my-backend/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:3000', 
-  'https://yc-zzz.github.io' 
+  'https://beattheblues.vercel.app/' 
 ];
 
 app.use(cors({
