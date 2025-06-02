5a9e5568d6f72da7beeed7c96095dea387d78629
yc-zzz <e1406485@u.nus.edu>
2025-06-02 11:28:21 +0800

    Update db.js to pull info from environmental variables

--- my-backend/db.js
+++ my-backend/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'insert_password',
-    database: 'auth_system',
+    host: 'process.env.DB_HOST',
+    user: 'process.env.DB_USER',
+    password: 'process.env.DB_PASS',
+    database: 'process.env.DB_NAME',
 });
 
 module.exports = db;
