055d697d6b81a8a2b8ad0f76802755510381fab4
yc-zzz <e1406485@u.nus.edu>
2025-06-12 16:26:43 +0800

    Update db.js to use PostgreSQL

--- my-backend/db.js
+++ my-backend/db.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2/promise');
+const { Pool } = require('pg');
 require('dotenv').config();
 
-const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Neon
+  },
 });
 
 module.exports = db;
