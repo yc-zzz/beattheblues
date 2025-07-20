a9b155388f3c719a2ae60da9d188158d3d8a532f
yc-zzz <e1406485@u.nus.edu>
2025-07-20 09:24:10 +0800

    Google login implemented in frontend and backend

--- my-backend/routes/auth.js
+++ my-backend/routes/auth.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import db from '../db.js';
+import {OAuth2Client} from 'google-auth-library';
+
 const router = express.Router();
+const client = new OAuth2Client('924199181328-6uvn8i1usu7b7j953l2jstrbrmp8sh01.apps.googleusercontent.com');
 //fyi 400 is bad request, 401 is unauthorised, 500 is server error
 //register route
 router.post('/register', async (req, res) => {
@@ -59,6 +62,37 @@ router.post('/login', async (req, res) => {
   catch (err) {
     console.error(err);
     res.status(500).json({success: false, message: 'Server error during login.'});
+  }
+});
+
+//google login route
+router.post('/auth/google', async(req, res) => {
+  const {token} = req.body;
+
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: '924199181328-6uvn8i1usu7b7j953l2jstrbrmp8sh01.apps.googleusercontent.com'
+    });
+    const payload = ticket.getPayload();
+    const email = payload.email;
+    const username = payload.name;
+
+    const result = await db.query(
+      'SELECT * FROM users WHERE email = $1',
+      [email]
+    );
+
+    if (result.rows.length === 0) {
+      await db.query(
+        'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)',
+        [username,email,null]
+      );
+    }
+    res.status(200).json({success:true, username});
+  } catch (e){
+    console.error(e);
+    res.status(401).json({success: false, message:'invalid google token'})
   }
 });
 
